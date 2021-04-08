a21dd27b0f3ed47c5924ad2a42e3dc1b67d4da65
Leonardo Teixeira <leovenancio00@gmail.com>
2021-04-08 18:50:48 -0300

    componente obter produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { MatInputModule } from '@angular/material/input';
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
-    RedDirective
+    RedDirective,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -25,6 +25,8 @@ export class ProductService {
     return this.http.post<Product>(this.baseUrl, product)
   }
 
-
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl)
+  }
 
 }
